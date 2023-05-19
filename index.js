8bb5dc2e6ec00dc17b1ddc24fab7dce434f3a287
Kilian Lamula <kilian.lamula@etud.univ-jfc.fr>
2023-05-19 16:26:28 +0300

    Db compare and synch

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import cors from 'cors'
 import express from 'express';
 import bodyParser from 'body-parser';
 import cron from 'node-cron';
+import axios from 'axios';
+import { dbconfig } from './dbconfig.js';
+import oracledb from 'oracledb';
 
 import adminRoutes from './routes/admin.js';
 import ownerRoutes from './routes/owner.js';
@@ -30,8 +33,64 @@ app.use('/housing', housingRoutes)
 app.use('/housingtypes', housingTypesRoutes)
 app.use('/message', messageRoutes)
 
-cron.schedule('*/1 * * * *', () => {
-  //gérer les étudiant
+const compareDatabases = async () => {
+  try {
+    //Students from second DB
+    const response = await axios.get('http://34.248.236.103:3000/students');
+    const studentsFromSecondDB = response.data;
+
+    //DB connection
+    await oracledb.createPool(dbconfig);
+    const connection = await oracledb.getConnection();
+
+    //Students from our DB
+    const query = `SELECT * FROM student`;
+    const result = await connection.execute(query);
+    const studentsFromYourDB = result.rows;
+
+    //Compare and synch
+    for (const studentFromSecondDB of studentsFromSecondDB) {
+      const matchingStudent = studentsFromYourDB.find(
+        (studentFromYourDB) => studentFromYourDB[0] == studentFromSecondDB[2]
+      );
+      if (matchingStudent) {
+        //The student exists, we update it :
+        const updateQuery = `UPDATE student SET first_name = :first_name, last_name = :last_name, email = :email WHERE student_id = :student_id`;
+        const updateParams = {
+          student_id: studentFromSecondDB[2],
+          first_name: studentFromSecondDB[3],
+          last_name: studentFromSecondDB[4],
+          email: studentFromSecondDB[5]
+        };
+        await connection.execute(updateQuery, updateParams);
+        //Student doesn't exist :
+      } else {
+        const insertQuery = `INSERT INTO student VALUES (:student_id, 'EUC', :student_id, :first_name, :last_name, :email, '0987654321')`;
+        const insertParams = {
+          student_id: studentFromSecondDB[2],
+          first_name: studentFromSecondDB[3],
+          last_name: studentFromSecondDB[4],
+          email: studentFromSecondDB[5]
+        };
+        await connection.execute(insertQuery, insertParams);
+      }
+    }
+    
+    //commit
+    await connection.commit();
+
+    //Close connection
+    await connection.close();
+    await oracledb.getPool().close();
+
+    console.log('Compare and sync completed');
+  } catch (error) {
+    console.error('Error comparing databases', error);
+  }
+};
+
+cron.schedule('*/30 * * * *', () => {
+  compareDatabases();
 });
 
 const PORT = process.env.PORT|| 5000;
